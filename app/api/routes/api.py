8a81d7f9f04470509876f88bd6c672f7aa02b46e
safwan-vk-at-646714546322 <safwan.vk@epixelsolutions.com>
2025-02-16 20:28:01 +0530

    Add crud, favorite, feed and unfavorite of favorites

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.routes import health_check, authentication, users, profiles
+from app.api.routes.articles import api as articles
 
 router = APIRouter()
 router.include_router(health_check.router, tags=["Health check"], prefix="/health-check")
 router.include_router(authentication.router, tags=["authentication"], prefix="/users")
 router.include_router(users.router, tags=["users"], prefix="/user")
 router.include_router(profiles.router, tags=["profiles"], prefix="/profiles")
+router.include_router(articles.router, tags=["articles"])
