c942c55f4c933bba34b6011c4e077d6ef56a41a2
safwan-vk-at-646714546322 <safwan.vk@epixelsolutions.com>
2025-02-16 18:47:50 +0530

    Add register and authenication

---
+++ app/api/routes/users.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from app.api.dependencies.authentication import get_current_user_authorizer
+from app.core import config
+from app.models.domain.users import User
+from app.models.schemas.users import UserInResponse, UserWithToken
+from app.services import jwt
+
+router = APIRouter()
+
+
+@router.get("", response_model=UserInResponse, name="users:get-current-user")
+async def retrieve_current_user(
+    user: User = Depends(get_current_user_authorizer()),
+) -> UserInResponse:
+    token = jwt.create_access_token_for_user(user, str(config.SECRET_KEY))
+    return UserInResponse(
+        user=UserWithToken(
+            username=user.username,
+            email=user.email,
+            bio=user.bio,
+            image=user.image,
+            token=token,
+        )
+    )
