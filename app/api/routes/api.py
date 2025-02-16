ffc0644ad15df848887c0b2c9a675f8750027931
safwan-vk-at-646714546322 <safwan.vk@epixelsolutions.com>
2025-02-16 19:08:33 +0530

    Add profile with follow and unfollow options

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import health_check, authentication, users
+from app.api.routes import health_check, authentication, users, profiles
 
 router = APIRouter()
 router.include_router(health_check.router, tags=["Health check"], prefix="/health-check")
 router.include_router(authentication.router, tags=["authentication"], prefix="/users")
 router.include_router(users.router, tags=["users"], prefix="/user")
-
-"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImV4cCI6MTc0MDMxNTEzMywic3ViIjoiYWNjZXNzIn0.jW9BLiY3hs_q3hUk-o4OJziwNmAMkI1LUiH-kFEaIwY"
+router.include_router(profiles.router, tags=["profiles"], prefix="/profiles")
